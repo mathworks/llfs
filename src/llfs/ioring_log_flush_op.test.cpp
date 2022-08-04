4a977d1fb73259a8d36966a8911ef3675de6c74a
Anthony Astolfi <tastolfi@gmail.com>
2022-08-04 09:35:50 -0400

    WIP - more IoRingLogFlushOp test.

--- src/llfs/ioring_log_flush_op.test.cpp
+++ src/llfs/ioring_log_flush_op.test.cpp
@@ -30,6 +30,12 @@ namespace {
 using namespace llfs::int_types;
 using namespace llfs::constants;
 
+using llfs::ConstBuffer;
+using llfs::Optional;
+using llfs::slot_offset_type;
+using llfs::Status;
+using llfs::StatusOr;
+
 // Test Plan:
 //
 //  Parameters:
@@ -67,26 +73,39 @@ struct IoRingLogFlushOpState : boost::equality_comparable<IoRingLogFlushOpState>
   bool done = false;
 };
 
+//=#=#==#==#===============+=+=+=+=++=++++++++++++++-++-+--+-+----+---------------
+//
 class MockDriver
 {
  public:
   MOCK_METHOD(const llfs::LogBlockCalculator&, calculate, (), (const));
 
-  MOCK_METHOD(llfs::slot_offset_type, get_trim_pos, (), (const));
+  MOCK_METHOD(slot_offset_type, get_trim_pos, (), (const));
 
-  MOCK_METHOD(llfs::slot_offset_type, get_flush_pos, (), (const));
+  MOCK_METHOD(slot_offset_type, get_flush_pos, (), (const));
 
-  MOCK_METHOD(llfs::slot_offset_type, get_commit_pos, (), (const));
+  MOCK_METHOD(slot_offset_type, get_commit_pos, (), (const));
 
-  MOCK_METHOD(llfs::ConstBuffer, get_data, (llfs::slot_offset_type slot_offset), (const));
+  MOCK_METHOD(ConstBuffer, get_data, (slot_offset_type slot_offset), (const));
 
   MOCK_METHOD(std::string_view, name, (), (const));
 
   MOCK_METHOD(usize, index_of_flush_op,
               (const llfs::BasicIoRingLogFlushOp<::testing::StrictMock<MockDriver>>* flush_op),
               (const));
+
+  MOCK_METHOD(void, async_write_some,
+              (i64 log_offset, const ConstBuffer& data, i32 buf_index,
+               std::function<void(StatusOr<i32>)> handler),
+              ());
+
+  MOCK_METHOD(void, wait_for_commit, (slot_offset_type least_upper_bound), ());
+
+  MOCK_METHOD(void, poll_flush_state, (), ());
 };
 
+// Returned by MockDriver::name();
+//
 const std::string_view kFakeDriverName = "TheFakeDriver";
 
 //=#=#==#==#===============+=+=+=+=++=++++++++++++++-++-+--+-+----+---------------
@@ -110,11 +129,16 @@ class IoRingLogFlushOpModel
     if (this->state_.is_terminal()) {
       return;
     }
+    auto on_scope_exit = batt::finally([&] {
+      this->state_.done = true;
+    });
 
     // Driver constants (test params).
     //
     const usize queue_depth = this->pick_one_of({1, 2, 4});
 
+    // The op-under-test is assigned to be either the first, last, or exact middle op in the queue.
+    //
     const usize op_index = [&]() -> usize {
       if (queue_depth == 0) {
         return 0;
@@ -145,9 +169,11 @@ class IoRingLogFlushOpModel
     ASSERT_EQ(1ull << pages_per_block_log2, pages_per_block);
 
     const usize block_size = llfs::kLogPageSize * pages_per_block;
-
     const usize block_capacity = block_size - sizeof(llfs::PackedLogPageHeader);
 
+    // Pick the log size; include the case where there are more flush ops than log capacity, so long
+    // as it doesn't lead to a "negative" log size.
+    //
     const usize log_size = [&] {
       if (block_capacity > llfs::kLogPageSize && this->pick_branch()) {
         return queue_depth * block_capacity - llfs::kLogPageSize;
@@ -161,8 +187,12 @@ class IoRingLogFlushOpModel
       }
     }();
 
+    // Start the log device blocks at file_offset=0 or some irregular large-ish value.
+    //
     const i64 base_file_offset = this->pick_one_of({0, 7777 * 512});
 
+    // We use the built-in calculator for the physical layout size.
+    //
     const u64 physical_size =
         llfs::LogBlockCalculator::disk_size_required_for_log_size(log_size, block_size);
 
@@ -170,6 +200,8 @@ class IoRingLogFlushOpModel
                  << BATT_INSPECT(block_capacity) << BATT_INSPECT(log_size)
                  << BATT_INSPECT(base_file_offset) << BATT_INSPECT(physical_size);
 
+    // Now the calculator has everything it needs.
+    //
     const llfs::LogBlockCalculator calculate{llfs::IoRingLogConfig{
                                                  .logical_size = log_size,
                                                  .physical_offset = base_file_offset,
@@ -181,9 +213,9 @@ class IoRingLogFlushOpModel
 
     // Driver variables (state).
     //
-    llfs::slot_offset_type trim_pos = 0;
-    llfs::slot_offset_type flush_pos = 0;
-    llfs::slot_offset_type commit_pos = 0;
+    slot_offset_type trim_pos = 0;
+    slot_offset_type flush_pos = 0;
+    slot_offset_type commit_pos = 0;
 
     // Create and configure the driver mock.
     //
@@ -213,6 +245,39 @@ class IoRingLogFlushOpModel
     op.emplace();
     op->initialize(&driver);
 
+    // The plan will be to simulate expected operating conditions of a real log.  We will simulate
+    // committing data, expect that flush operations (writes) start (once the commit_pos is in the
+    // op-under-test's active slot range), simulate the completion of the writes (full success,
+    // partial (short) success, and failure), and in the case of failure, we will simulate a crash
+    // by destructing the op and re-initializing as though we are in recovery.  The limit for
+    // simulated log writes will be to wrap around the full capacity of the log twice.
+    //
+    const slot_offset_type max_flush_pos = log_size * 2 + 1;
+    const usize max_failures = 2;
+
+    usize failure_count = 0;
+    slot_offset_type expected_wait_commit_pos = op_index * block_capacity + 1;
+    {
+      EXPECT_CALL(driver, wait_for_commit(expected_wait_commit_pos))  //
+          .WillOnce(::testing::Return());
+
+      op->activate();
+    }
+    Optional<slot_offset_type> actual_wait_commit_pos = expected_wait_commit_pos;
+
+    while (failure_count < max_failures && flush_pos < max_flush_pos) {
+      const slot_offset_type old_trim_pos = trim_pos;
+      const slot_offset_type old_flush_pos = flush_pos;
+      const slot_offset_type old_commit_pos = commit_pos;
+      const usize old_failure_count = failure_count;
+
+      // If we fail to make any kind of progress, end the simulation.
+      //
+      if (trim_pos == old_trim_pos && flush_pos == old_flush_pos && commit_pos == old_commit_pos &&
+          failure_count == old_failure_count) {
+        break;
+      }
+    }
     /*
     this->do_one_of(
         [] {

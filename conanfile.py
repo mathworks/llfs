988df3b654ee4d28b5f8cc50a5e167631ede3255
Anthony Astolfi <tastolfi@gmail.com>
2022-10-28 17:07:52 -0400

    Clean up conanfile (use set_version as recommended).

--- conanfile.py
+++ conanfile.py
@@ -10,19 +10,12 @@ from conans import ConanFile, CMake
 
 import os, sys
 
-VERBOSE_ = os.getenv('VERBOSE') and True or False
+VERBOSE = os.getenv('VERBOSE') and True or False
 
-SCRIPT_DIR_ = os.path.join(os.path.dirname(__file__), 'batteries', 'script')
-if VERBOSE_:
-    print(f"SCRIPT_DIR_={SCRIPT_DIR_}", file=sys.stderr)
-
-VERSION_ = os.popen("VERBOSE= " + os.path.join(SCRIPT_DIR_, "get-version.sh")).read().strip()
-if VERBOSE_:
-    print(f"VERSION_={VERSION_}", file=sys.stderr)
 
 class LlfsConan(ConanFile):
     name = "llfs"
-    version = VERSION_
+    # version is set automatically from Git tags - DO NOT SET IT HERE
     license = "Apache Public License 2.0"
     author = "The MathWorks, Inc."
     url = "https://github.com/mathworks/llfs"
@@ -38,7 +31,7 @@ class LlfsConan(ConanFile):
         "openssl/3.0.3",
         "glog/0.6.0",
         "libunwind/1.5.0",
-        "batteries/0.12.0@batteriescpp+batteries/stable",
+        "batteries/0.12.5@batteriescpp+batteries/stable",
         "liburing/2.1",
         "cli11/1.9.1",
     ]
@@ -49,20 +42,40 @@ class LlfsConan(ConanFile):
         "src/**/*.cpp",
     ]
 
+    def set_version(self):
+        #==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+        # Import the Batteries utility module.
+        #
+        script_dir = os.path.join(os.path.dirname(__file__), 'batteries', 'script')
+        sys.path.append(script_dir)
+        
+        import batt
+        
+        batt.VERBOSE = VERBOSE
+        self.version = batt.get_version(no_check_conan=True)
+        batt.verbose(f'VERSION={self.version}')
+        #
+        #+++++++++++-+-+--+----- --- -- -  -  -   -        
+    
     def configure(self):
         self.options["gtest"].shared = False
         self.options["boost"].shared = False
 
     def build(self):
         cmake = CMake(self)
-        cmake.verbose = VERBOSE_
+        cmake.verbose = VERBOSE
         cmake.configure(source_folder="src")
         cmake.build()
+
+    def export(self):
+        self.copy("*.sh", src="batteries/script", dst="batteries/script")
+        self.copy("*.py", src="batteries/script", dst="batteries/script")
 
     def package(self):
         self.copy("*.hpp", dst="include", src="src")
         self.copy("*.ipp", dst="include", src="src")
-        self.copy("*.sh", dst="bin", src="script")
+        self.copy("*.sh", src="batteries/script", dst="batteries/script")
+        self.copy("*.py", src="batteries/script", dst="batteries/script")
         cmake = CMake(self)
         cmake.configure()
         cmake.install()
@@ -71,6 +84,3 @@ class LlfsConan(ConanFile):
         self.cpp_info.cxxflags = ["-std=c++17", "-D_GNU_SOURCE"]
         self.cpp_info.system_libs = ["dl"]
         self.cpp_info.libs = ["llfs"]
-
-    def package_id(self):
-        pass

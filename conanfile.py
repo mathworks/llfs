4ce7fe05f7298302b0ce7a93d220588bfc7c2073
Anthony Astolfi <tastolfi@gmail.com>
2023-02-06 14:28:01 -0500

    Upgrade 3p dependencies.

--- conanfile.py
+++ conanfile.py
@@ -26,12 +26,12 @@ class LlfsConan(ConanFile):
     generators = "cmake"
     build_policy = "missing"
     requires = [
-        "gtest/1.12.1",
-        "boost/1.79.0",
+        "gtest/1.13.0",
+        "boost/1.81.0",
         "openssl/3.0.3",
         "glog/0.6.0",
         "libunwind/1.5.0",
-        "batteries/0.20.1@batteriescpp+batteries/stable",
+        "batteries/0.23.1@batteriescpp+batteries/stable",
         "liburing/2.1",
         "cli11/1.9.1",
         "zlib/1.2.12",

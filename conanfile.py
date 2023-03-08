d76240e2613ddcdc0cdf4945067d7b20aa7aa77b
Anthony Astolfi <tastolfi@gmail.com>
2023-03-08 14:38:26 -0500

    Upgrade to batteries/0.27.3.

--- conanfile.py
+++ conanfile.py
@@ -31,7 +31,7 @@ class LlfsConan(ConanFile):
         "openssl/3.0.7",
         "glog/0.6.0",
         "libunwind/1.6.2",
-        "batteries/0.25.4@batteriescpp+batteries/stable",
+        "batteries/0.27.3@batteriescpp+batteries/stable",
         "liburing/2.2",
         "cli11/2.3.2",
         "zlib/1.2.13",

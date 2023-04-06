8d6cd68868a54debde5a5f8aafa0e6cdf1e390af
Anthony Astolfi <tastolfi@gmail.com>
2023-04-06 17:53:37 -0400

    Implement StorageSimulation::get_page_device.

--- src/llfs/storage_simulation.cpp
+++ src/llfs/storage_simulation.cpp
@@ -11,6 +11,8 @@
 
 #include <llfs/simulated_log_device.hpp>
 #include <llfs/simulated_log_device_impl.hpp>
+#include <llfs/simulated_page_device.hpp>
+#include <llfs/simulated_page_device_impl.hpp>
 
 namespace llfs {
 
@@ -82,6 +84,36 @@ std::unique_ptr<LogDevice> StorageSimulation::get_log_device(const std::string& 
   BATT_CHECK_NOT_NULLPTR(iter->second);
 
   return std::make_unique<SimulatedLogDevice>(batt::make_copy(iter->second));
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+std::unique_ptr<PageDevice> StorageSimulation::get_page_device(const std::string& name,
+                                                               Optional<PageCount> page_count,
+                                                               Optional<PageSize> page_size)
+{
+  auto iter = this->page_devices_.find(name);
+
+  // If we didn't find the named device, then create it.
+  //
+  if (iter == this->page_devices_.end()) {
+    BATT_CHECK(page_count && page_size)
+        << "Must specify page count/size if creating a simulated page device for the first time!";
+
+    const page_device_id_int next_page_device_id = this->page_devices_.size();
+
+    iter = this->page_devices_
+               .emplace(name, std::make_shared<SimulatedPageDevice::Impl>(
+                                  *this, *page_size, *page_count, next_page_device_id))
+               .first;
+  }
+
+  // At this point we should have a valid entry.
+  //
+  BATT_CHECK_NE(iter, this->page_devices_.end());
+  BATT_CHECK_NOT_NULLPTR(iter->second);
+
+  return std::make_unique<SimulatedPageDevice>(batt::make_copy(iter->second));
 }
 
 }  //namespace llfs

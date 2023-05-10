3873932a0dcea7a20fdfa92add5b879d31a243c4
Anthony Astolfi <tastolfi@gmail.com>
2023-05-10 11:07:56 -0400

    Add option to delay flush (manual vs auto) in MemoryLogDevice.

--- src/llfs/memory_log_device.cpp
+++ src/llfs/memory_log_device.cpp
@@ -14,6 +14,80 @@
 namespace llfs {
 
 //#=##=##=#==#=#==#===#+==#+==========+==+=+=+=+=+=++=+++=+++++=-++++=-+++++++++++
+// class MemoryLogStorageDriver
+//
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+bool MemoryLogStorageDriver::is_auto_flush() const noexcept
+{
+  return this->flush_pos_ == &this->commit_pos_;
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+void MemoryLogStorageDriver::set_auto_flush(bool on) noexcept
+{
+  if (on == this->is_auto_flush()) {
+    // No change.
+    //
+    return;
+  }
+
+  this->sync_flush_pos();
+  if (on) {
+    this->flush_pos_ = &this->commit_pos_;
+  } else {
+    this->flush_pos_ = &this->manual_flush_pos_;
+  }
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+void MemoryLogStorageDriver::sync_flush_pos() noexcept
+{
+  this->manual_flush_pos_.set_value(this->commit_pos_.get_value());
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+usize MemoryLogStorageDriver::unflushed_size() noexcept
+{
+  if (this->is_auto_flush()) {
+    return 0;
+  }
+  return slot_distance(this->get_flush_pos(), this->get_commit_pos());
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+isize MemoryLogStorageDriver::advance_flush_pos(isize offset_delta) noexcept
+{
+  if (this->is_auto_flush()) {
+    return 0;
+  }
+  offset_delta = slot_max(0, offset_delta);
+  offset_delta = slot_min(offset_delta, this->unflushed_size());
+  this->manual_flush_pos_.fetch_add(offset_delta);
+  return offset_delta;
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+slot_offset_type MemoryLogStorageDriver::flush_up_to_offset(slot_offset_type offset) noexcept
+{
+  if (this->is_auto_flush()) {
+    return this->get_flush_pos();
+  }
+
+  // Don't ever go past the commit_pos!
+  //
+  offset = slot_min(offset, this->get_commit_pos());
+
+  return clamp_min_slot(this->manual_flush_pos_, offset);
+}
+
+//#=##=##=#==#=#==#===#+==#+==========+==+=+=+=+=+=++=+++=+++++=-++++=-+++++++++++
 // class MemoryLogDevice
 //
 

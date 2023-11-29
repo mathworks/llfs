fd5128201a5c704dad75662839b2ab7a1097e2ce
Anthony Astolfi <tastolfi@gmail.com>
2023-11-29 15:18:54 -0500

    Upgrade to batteries/0.49.1.

--- conanfile.py
+++ conanfile.py
@@ -52,7 +52,7 @@ class LlfsConan(ConanFile):
             "gtest/1.14.0",
             "boost/1.83.0",
             "glog/0.6.0",
-            "batteries/0.48.7",
+            "batteries/0.49.1",
             "cli11/2.3.2",
         ]
 

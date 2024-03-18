997bc300f6f0ce077041c811b9fa7c8b6e7a981c
Anthony Astolfi <tastolfi@gmail.com>
2024-03-18 10:39:27 -0400

    Add Fragment::push(Fragment) overloads.

--- src/llfs/ioring_stream_buffer.cpp
+++ src/llfs/ioring_stream_buffer.cpp
@@ -239,6 +239,24 @@ void IoRingStreamBuffer::Fragment::push(BufferView&& view)
 
 //==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
 //
+void IoRingStreamBuffer::Fragment::push(const Fragment& fragment)
+{
+  for (const BufferView& part : fragment) {
+    this->push(part);
+  }
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
+void IoRingStreamBuffer::Fragment::push(Fragment&& fragment)
+{
+  for (BufferView& part : fragment) {
+    this->push(std::move(part));
+  }
+}
+
+//==#==========+==+=+=++=+++++++++++-+-+--+----- --- -- -  -  -   -
+//
 auto IoRingStreamBuffer::Fragment::pop(usize max_byte_count) -> Fragment
 {
   usize bytes_popped = 0;
